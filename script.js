44b856ced60a89913d92bf3b0ad8d31164494a78
Killex8569-pro <alexandre.faublanette@gmail.com>
2024-10-12 12:30:09 +0200

    premier test
    just a test

---
+++ script.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      if (sectionTop < window.innerHeight - 100) {
+        section.classList.add('visible');
+      } else {
+        section.classList.remove('visible');
+      }
+    });
+  });
+  
